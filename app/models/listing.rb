3b9af87147c694ac105b3520326a2d5b90dc991c
Daniel Suarez <suarez.daniel101@gmail.com>
2022-11-08 17:13:18 -0500

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,12 @@
 class Listing < ActiveRecord::Base
+  validates :address, presence: true
+  validates :listing_type, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :neighborhood, presence: true
+
+  
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
   has_many :reservations
